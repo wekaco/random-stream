466ffe2f04edc1ebdfd5ea699a0f620643e184d7
Dmtrs Flaco Mengidis <tydeas.dr@gmail.com>
2020-01-24 02:07:15 +0200

    feat(stems): Multiple stems support

--- src/app.js
+++ src/app.js
@@ -3,12 +3,16 @@ import App from './app.riot';
 
 const createApp = component(App);
 
+const root = `data/PALLADOS05`;
+const path = (file) => `${root}/${file}`;
+
 const oReq = new XMLHttpRequest();
 oReq.addEventListener('load', function load() {
-  const { master } = JSON.parse(this.responseText);
+  const { master, stems } = JSON.parse(this.responseText);
   createApp(document.getElementById('root'), {
-    master: `data/${master}`,
+    stems: stems.map(path),
+    master: path(master),
   });
 });
-oReq.open('GET', 'data/PALLADOS05/manifest.json');
+oReq.open('GET', path('manifest.json'));
 oReq.send();
